fc05641a6f57034d0c5ff39cddcdd16b63bc04ca
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2023-08-11 14:48:13 +0530

    added swagger doc and minor changes

--- pre-start.sh
+++ pre-start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Tagging and running the image
-nerdctl build -t gitskill .
+# Tagging and running the image locally
+nerdctl build --platform linux/amd64  -t gitskill .
+
 nerdctl run -d --platform linux/amd64 -p 8000:8000 -e ACCESS_TOKEN=$ACCESS_TOKEN gitskill
-
-
-# nerdctl tag gitskill  icr.io/bravehearts/gitskill:2.0
-# nerdctl push icr.io/bravehearts/gitskill
