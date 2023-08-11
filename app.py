6c87b22d3c555807912d087dc50ce8266556ea13
Akash Kumar <akash.kumar@ibm.com>
2023-08-11 15:36:53 +0530

    added APIs get_user_contribution and fixed get_total_pr_count

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from fastapi import FastAPI
 from fastapi import Response
 import uvicorn
 import logging
+from datetime import datetime, timedelta
+from tabulate import tabulate
 
 #
 # Configure logging
@@ -32,12 +34,11 @@ logger.addHandler(handler)
 
 app = FastAPI()
 
-
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", None)
 # REPO_OWNER = os.getenv("REPO_OWNER", None)
 # ORG_NAME = os.getenv("ORG_NAME", None)
-
 # GITHUB_API_URL = 'https://api.github.com/users/'
+
 #TODO: Make these inputs like Repo owner etc as Env vars
 GITHUB_API_BASE = "https://api.github.com"
 REPO_OWNER = "Ak-sky"
@@ -92,63 +93,99 @@ def total_commits(username: str):
     else:
         logger.error( "User does not exists.")
         return {"error": "User not found"}
-    
 
-@app.get('/get_collaborator_commits')
-def get_collaborator_commits(username: str):
-    url = f'https://api.github.com/orgs/{ORG_NAME}/repos'
-    headers = {
-    'Authorization': f'Token {ACCESS_TOKEN}'
-    }
-    total_commits_weekly = 0
-    logger.info( "Getting collab commits..")
-
-    response = requests.get(url, headers=headers)
-    repos = response.json()
-    
-    for repo in repos:
-        repo_name = repo['name']
-        commits_url = f'https://api.github.com/repos/{ORG_NAME}/{repo_name}/stats/contributors'
-        
-        response = requests.get(commits_url, headers=headers)
-        repo_stats = response.json()
-        logger.info(f"Repo stats : {repo_stats}")
-        for contributor in repo_stats:
-            if contributor['author']['login'] == username:
-                print(contributor['author']['login'])
-                total_commits_weekly += sum(week['c'] for week in contributor['weeks'])
-                break
-
-    if total_commits_weekly is not None:
-        logger.info("Success: Collaborator commits")
-        return {"contributor": contributor['author']['login'], "total_commits_weekly": total_commits_weekly}
-    else:
-        logger.error( "User does not exists")
-        return {"error": "User not found"}
 
 @app.get('/get_total_pr_count')
 def get_total_pr_count(username: str):
     headers = {
-    'Authorization': f'Token {ACCESS_TOKEN}'
-        }
-    logger.info( "GETTING TOTAL PR COUNT..")
-    
-    query = f"author:{username} is:pr"
+        'Authorization': f'Bearer {ACCESS_TOKEN}'
+    }
 
-    search_url = f"{GITHUB_API_BASE}/search/issues?q={query}"
+    url = f'https://api.github.com/search/issues?q=type:pr+author:{username}'
     
-    response = requests.get(search_url, headers=headers)
-    if response.status_code == 200:
-        logger.info( "PR COUNT : Success")
-        data = response.json()
-        total_count = data.get("total_count", 0)
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    total_pr_count = data['total_count']
     
-    if total_count is not None:
-        logger.info( "Invalid PR Count")
-        return {"total_PR_count": total_count}
+    if total_pr_count is not None:
+        logger.info( "Successfully recieved total PR count.")
+        return {"username": username, "total_pr_count": total_pr_count}
     else:
-        logger.error( "User not found")
+        logger.error( "User does not exists.")
         return {"error": "User not found"}
+    
+
+@app.get('/get_user_issues')
+def get_user_issues(username: str):
+    print("get in")
+
+    # base_url = f'https://api.github.com/users/{username}/issues'
+    response = requests.get(f'https://api.github.com/users/{username}/issues?filter=subscribed')
+    print(response)
+    
+    # if response.status_code == 200:
+    #     issues = response.json()
+    #     print(issues)
+    #     return {"Total issues raised": issues}
+    # else:
+    #     return {"Total issues raised": issues}
+    
+    # try:
+    #     response = requests.get(f"{GITHUB_API_BASE}/repos/{username}/{REPO_NAME}/issues", params={"creator": user})
+        
+    #     if response.status_code == 200:
+    #         issues = response.json()
+    #         return jsonify(issues)
+    #     else:
+    #         return jsonify({"error": "Failed to retrieve issues"}), response.status_code
+    # except Exception as e:
+    #     return jsonify({"error": str(e)}), 500
+
+git_header = headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN}'
+    }
+git_url = "https://api.github.com/graphql"
+
+def run_query(url, header, query, variables):
+    request = requests.post(url, json={'query': query, 'variables': variables}, headers=header)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+
+query = '''
+    query($username: String!, $from : DateTime!, $to: DateTime! ) {
+        user(login: $username) { 
+            contributionsCollection(from: $from, to: $to) {
+                contributionCalendar {
+                    totalContributions
+                }
+            }
+        }
+    }
+'''
+@app.get('/get_user_contribution')
+def get_user_contribution(username: str, range_val: int):
+    users = [username]  #List of your organization users
+    user_insights = [] 
+    range=range_val #Date range to retrieve contributions
+    now_str = (datetime.now()).strftime("%Y-%m-%dT%H:%M:%S")
+    n_days_ago_str = (datetime.now() - timedelta(days=range)).strftime("%Y-%m-%dT%H:%M:%SZ")
+    input_variables = {}
+    input_variables['from'] = n_days_ago_str
+    input_variables['to'] = now_str
+
+    for user in users:
+        input_variables['username'] = user
+        resp_data = run_query(git_url, git_header, query, input_variables)
+        user_contributions  = resp_data['data']['user']['contributionsCollection']['contributionCalendar']['totalContributions']
+        user_insights.append([user, user_contributions])        
+
+    # head = ["User", "Contributions"]
+    # print(tabulate(user_insights, headers=head, tablefmt="grid"))
+    return {"user": user_insights[0][0], "contribution": user_insights[0][1]}
 
 
 
